60e75c48f5f04f42d7b3099e3595b6dfeb334475
demic <59309595+demic-dev@users.noreply.github.com>
2026-04-03 16:38:57 +0200

    fixed pdf a standard

--- shell.nix
+++ shell.nix
@@ -9,10 +9,10 @@ pkgs.mkShell {
 
   shellHook = ''
     echo "Compiling..."
-    typst compile thesis.typ thesis.raw.pdf --pdf-standard a-4f
-    typst compile summary.typ summary.raw.pdf --pdf-standard a-4f
+    typst compile thesis.typ thesis.raw.pdf --pdf-standard 1.4
+    typst compile summary.typ summary.raw.pdf --pdf-standard a-1b
     echo "Compressing..."
-    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=thesis.pdf thesis.raw.pdf
+    gs -dPDFA=2 -dNOOUTERSTREAM -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=thesis.pdf thesis.raw.pdf
     gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=summary.pdf summary.raw.pdf
     echo "Cleaning up..."
     rm thesis.raw.pdf
